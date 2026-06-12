a22408189513e556758269b341bd2a4409bdec09
alikseich <gukaiandrei2@gmail.com>
2026-06-12 23:44:16 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,9 @@
-const CACHE_NAME = 'liferpg-nexus-v4';
+const CACHE_NAME = 'liferpg-nexus-v5';
 
-// Установка: не ждем, активируем сразу
 self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
-// Активация: Убиваем абсолютно все старые кэши, чтобы не было белых экранов
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
@@ -17,7 +15,6 @@ self.addEventListener('activate', event => {
   self.clients.claim();
 });
 
-// Запрос сети: Всегда идем в интернет за свежим кодом
 self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request).catch(() => {
